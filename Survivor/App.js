2e644cffe0dcce677426a99f5358c8ccb313ea0a
matthias <matthias.gaste@epitech.eu>
2023-09-06 10:14:26 +0200

    build(Application): add Survivor folder with the base of the application inside

---
+++ Survivor/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World, you are ready to start to work!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
