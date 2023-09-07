ff759c860030d20f78be1c03969736133905ac7c
Jibea <87119218+Jibea@users.noreply.github.com>
2023-09-07 09:45:35 +0200

    [trombinoscope] add a trombinoscope page in app (#7)
    * feat(trombinoscope): add a first version of trombinoscope
    
    * style(Trombinoscope): change the style of image and text display

---
+++ Survivor/Pages/DrawerMenu.js
@@ -0,0 +1,54 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import TrombinoscopeScreen from './Trombinoscope.js';
+
+import { Ionicons } from '@expo/vector-icons';
+import { Button, View, StyleSheet } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+export default function DrawerMenu({ navigation }) {
+  return (
+    <NavigationContainer styles={style.container}>
+      <Drawer.Navigator initialRouteName="trombinoscope">
+      <Drawer.Screen name="Trombinoscope" component={TrombinoscopeScreen}
+          options={
+            {
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                name= {focused ? "people": "people-outline"}
+                  size={size}
+                  color={focused ? 'blue' : '#ccc'}
+                />
+              ),
+            }} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen}
+          options={
+            {
+              drawerIcon: ({ focused, size }) => (
+                <Ionicons
+                  name="ios-notifications"
+                  size={size}
+                  color={focused ? 'blue' : '#ccc'}
+                />
+              ),
+            }} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const style = ({
+  container: {
+    flex: 1,
+    backgroundColor: '#E5E7E6',
+  },
+});
