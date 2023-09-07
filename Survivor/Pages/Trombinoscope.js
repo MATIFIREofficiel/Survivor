36be5c11b872448460701c20e6dc2706eac6b8ed
Jibea <87119218+Jibea@users.noreply.github.com>
2023-09-07 19:40:29 +0200

    [profile page] add a profile page (#12)
    * feat(dependencies and modules): add a profile page
    
    * feat(ProfilePage): add the js files for ProfilePage
    
    * feat(trombinoscope): fix the number of pages displayed

--- Survivor/Pages/Trombinoscope.js
+++ Survivor/Pages/Trombinoscope.js
@@ -1,113 +1,106 @@
-import { View, Text, Button, Image, FlatList , StyleSheet} from "react-native";
+import { View, Text, Button, Image, FlatList, StyleSheet, useWindowDimensions} from "react-native";
 import axios from "axios";
-import React, { useState, memo , useEffect, route} from 'react';
+import React, { useState, memo, useEffect, route } from 'react';
 
-function Item({ item, onPress }) {
-    return (
-        <>
-            <View>
-                <Image
-                    source={{
-                        uri: `https://masurao.fr/api/employees/${item.id}/image`,
-                        method: 'GET',
-                        headers: {
-                            accept: 'application/json',
-                            'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
-                            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k',
-                        },
-                    }}
-                    style={styles.image} />
-                <Text style={styles.item}> {item.name} {item.surname} </Text>
-            </View>
-        </>
-    );
+function Item({ item, onPress, columnCount }) {
+  const screenWidth = useWindowDimensions().width;
+  const itemWidth = screenWidth / columnCount - 20
+  return (
+    <>
+      <View>
+        <Image
+          source={{
+            uri: `https://masurao.fr/api/employees/${item.id}/image`,
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k',
+            },
+          }}
+          style={styles.image} />
+        <Text style={styles.item}> {item.name} {item.surname} </Text>
+      </View>
+    </>
+  );
 }
 
 const ids = [];
 
 
-export default function TrombinoscopeScreen({ navigation}) {
-    const [refreshing, setRefreshing] = useState(false);
+export default function TrombinoscopeScreen({ navigation }) {
 
-    const [infos, setinfos] = useState([]);
+  const [infos, setinfos] = useState([]);
 
-    const getListEmployeesID = async () => {
+  const screenWidth = useWindowDimensions().width;
+  const columnCount = screenWidth >= 400 ? 3 : 2;
+  const getListEmployeesID = async () => {
 
-        const url = 'https://masurao.fr/api/employees';
-        const headers = {
-            'accept': 'application/json',
-            'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k'
-        };
-
-        try {
-            const response = await axios.get(url, { headers });
-            setinfos(response.data);
-        } catch (error) {
-            console.log(error.accept);
-            return [];
-        }
+    const url = 'https://masurao.fr/api/employees';
+    const headers = {
+      'accept': 'application/json',
+      'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k'
     };
 
-    const handleButtonPress = async () => {
-        handleRefresh();
-    };
-    useEffect(() => {
-        getListEmployeesID();
-    }, []);
+    try {
+      const response = await axios.get(url, { headers });
+      setinfos(response.data);
+    } catch (error) {
+      console.log(error.accept);
+      return [];
+    }
+  };
 
-    const handleRefresh = () => {
-        setRefreshing(true);
-        console.log("refreshing");
-        setRefreshing(false);
-    };
+  useEffect(() => {
+    getListEmployeesID();
+  }, []);
 
-    const renderItem = ({ item }) => {
-        return (
-            <Item
-                item={item}
-                onPress={() => console.log("pressed")}
-            />
-        );
-    };
-
+  const renderItem = ({ item }) => {
     return (
+      <Item
+        item={item}
+        onPress={() => console.log("pressed")}
+        columnCount={columnCount}
+      />
+    );
+  };
 
-        <View style={styles.homePage}>
-            <Button
-                title="test"
-                onPress={handleButtonPress}
-            />
+  return (
 
-            <FlatList
-                refreshing={true}
-                data={infos}
-                renderItem={renderItem}
-                numColumns={2}
-                extraData={infos}
-            />
+    <View style={styles.homePage}>
+      <FlatList
+        style={styles.list}
+        refreshing={true}
+        data={infos}
+        renderItem={renderItem}
+        numColumns={columnCount}
+        extraData={infos}
+      />
 
-        </View>
-    )
+    </View>
+  )
 }
 
-const styles = StyleSheet.create ({
-    homePage: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    item: {
-        paddingBottom: 10,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        width: 100,
-    },
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 40,
-        overflow:"hidden",
-        padding : 10,
-    },
+const styles = StyleSheet.create({
+  homePage: {
+    flex: 1,
+  },
+  list: {
+    marginTop: 10,
+  },
+  item: {
+    marginVertical: 8,
+    marginHorizontal: this.columnCount === 2 ? 50 : 16,
+    width: 110,
+  },
+  image: {
+    paddingLeft: this.columnCount === 2 ? 50 : 16,
+    marginHorizontal: this.columnCount === 2 ? 50 : 16,
+    marginTop: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 40,
+    overflow: "hidden",
+  },
 });
