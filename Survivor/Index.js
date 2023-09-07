b01e93e3c0a9e22959d065f50a560125ff6eea51
matthias <matthias.gaste@epitech.eu>
2023-09-07 10:37:18 +0200

    feat(Index): redirection of the user connection

---
+++ Survivor/Index.js
@@ -0,0 +1,14 @@
+import DrawerMenu from './Pages/DrawerMenu';
+import LoginPage from './Pages/LoginPage';
+import React, { useState } from 'react';
+
+export default function ConnectionChecker()
+{
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    if (isSignedIn) {
+        return (<DrawerMenu/>);
+    } else {
+        return (<LoginPage setIsSignedIn={setIsSignedIn} />);
+    }
+}
