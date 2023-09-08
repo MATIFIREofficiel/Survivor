06fb9f81a15b602efa4722db3f39e6bba22ff8e1
Jibea <guylain.thomas@epitech.eu>
2023-09-08 12:25:20 +0200

    feat(Trombinoscope): add a profileDetailsScreen to display datas

--- Survivor/Pages/DrawerMenu.js
+++ Survivor/Pages/DrawerMenu.js
@@ -1,5 +1,8 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import ProfileDetailScreen from './ProfileDetailsScreen.js';
 import TrombinoscopeScreen from './Trombinoscope.js';
 import ProfilePage from './ProfilePage.js';
 
@@ -16,16 +19,36 @@ function NotificationsScreen({ navigation }) {
   );
 }
 
-export default function DrawerMenu({ navigation , apiUser} ) {
+
+const Stack = createStackNavigator();
+
+function ProfileStack({route}) {
+  return (
+    <Stack.Navigator initialRouteName="Trombinoscope">
+      <Stack.Screen name="All members" component={TrombinoscopeScreen}
+        initialParams={
+          {
+            access_token: route.params,
+          }
+        }
+      />
+      <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
+    </Stack.Navigator>
+  );
+}
+
+
+export default function DrawerMenu({ navigation, apiUser }) {
   return (
     <NavigationContainer styles={style.container}>
       <Drawer.Navigator initialRouteName="trombinoscope">
-      <Drawer.Screen name="Trombinoscope" component={TrombinoscopeScreen}
+        <Drawer.Screen name="Trombinoscope" component={ProfileStack}
+          initialParams={apiUser}
           options={
             {
               drawerIcon: ({ focused, size }) => (
                 <Ionicons
-                name= {focused ? "people": "people-outline"}
+                  name={focused ? "people" : "people-outline"}
                   size={size}
                   color={focused ? 'blue' : '#ccc'}
                 />
@@ -43,7 +66,7 @@ export default function DrawerMenu({ navigation , apiUser} ) {
               ),
             }} />
         <Drawer.Screen name="Profile" component={ProfilePage}
-          initialParams={apiUser}
+          initialParams={[apiUser, 74]}
           options={
             {
               drawerIcon: ({ focused, size }) => (
--- Survivor/Pages/Trombinoscope.js
+++ Survivor/Pages/Trombinoscope.js
@@ -1,6 +1,6 @@
-import { View, Text, Button, Image, FlatList, StyleSheet, useWindowDimensions} from "react-native";
+import { View, Text, TouchableOpacity, Image, FlatList, StyleSheet, useWindowDimensions } from "react-native";
 import axios from "axios";
-import React, { useState, memo, useEffect, route } from 'react';
+import React, { useState, useEffect, route } from 'react';
 
 function Item({ item, onPress, columnCount }) {
   const screenWidth = useWindowDimensions().width;
@@ -8,30 +8,32 @@ function Item({ item, onPress, columnCount }) {
   return (
     <>
       <View>
-        <Image
-          source={{
-            uri: `https://masurao.fr/api/employees/${item.id}/image`,
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k',
-            },
-          }}
-          style={styles.image} />
-        <Text style={styles.item}> {item.name} {item.surname} </Text>
+        <TouchableOpacity onPress={onPress} style={styles.item}>
+          <Image
+            source={{
+              uri: `https://masurao.fr/api/employees/${item.id}/image`,
+              method: 'GET',
+              headers: {
+                accept: 'application/json',
+                'X-Group-Authorization': 'AhSyHQjIpPhIvfI5OU8HHe9nyhPKwY-q',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQsImVtYWlsIjoib2xpdmVyLmxld2lzQG1hc3VyYW8uanAiLCJuYW1lIjoiT2xpdmVyIiwic3VybmFtZSI6Ikxld2lzIiwiZXhwIjoxNjk1NzI2MzMwfQ.kfZoJqTF7H6Wg1egKTyA8W3r-pucZvkmvBAP0v_Fb6k',
+              },
+            }}
+            style={styles.image} />
+          <Text style={styles.item}> {item.name} {item.surname} </Text>
+        </TouchableOpacity>
       </View>
     </>
   );
 }
+let access_token = ""
 
-const ids = [];
 
-
-export default function TrombinoscopeScreen({ navigation }) {
+export default function TrombinoscopeScreen({ navigation, route }) {
 
   const [infos, setinfos] = useState([]);
 
+  const {access_token} = route.params;
   const screenWidth = useWindowDimensions().width;
   const columnCount = screenWidth >= 400 ? 3 : 2;
   const getListEmployeesID = async () => {
@@ -60,7 +62,8 @@ export default function TrombinoscopeScreen({ navigation }) {
     return (
       <Item
         item={item}
-        onPress={() => console.log("pressed")}
+        onPress={() =>
+          navigation.navigate('ProfileDetail', [access_token, item.id])}
         columnCount={columnCount}
       />
     );
